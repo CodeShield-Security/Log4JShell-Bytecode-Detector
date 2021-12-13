ba17ceca7739e3cbf066d5abc5b4b76e686c2e8d
Johannes Spaeth <johannes.spaeth@codeshield.de>
2021-12-13 09:53:02 +0100

    Adding tests

--- src/test/java/de/codeshield/log4jshell/Log4JDetectorTests.java
+++ src/test/java/de/codeshield/log4jshell/Log4JDetectorTests.java
@@ -14,11 +14,14 @@ public class Log4JDetectorTests
     @Test
     public void checkVulnerables() throws IOException, URISyntaxException {
       assertTrue(checkResourceFile("/en16931-xml-validator-2.0.0-b2-jar-with-dependencies.jar"));
+      assertTrue(checkResourceFile("/log4j-core-2.12.1.jar"));
+      assertTrue(checkResourceFile("/log4j-core-2.14.1.jar"));
     }
 
     @Test
     public void checkSecure() throws IOException, URISyntaxException {
       assertFalse(checkResourceFile("/spring-boot-2.5.7.jar") );
+      assertFalse(checkResourceFile("/log4j-core-2.15.0.jar"));
     }
 
     private boolean checkResourceFile(String url) throws IOException, URISyntaxException {
