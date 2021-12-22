a63ec5ae22bb0b508de8ca69c4082985540b2e02
Johannes Spaeth <johannes.spaeth@codeshield.de>
2021-12-22 19:52:41 +0100

    Removing incorrectly flagged class files due to revert in log4j project

--- src/test/java/de/codeshield/log4jshell/Log4JDetectorTests.java
+++ src/test/java/de/codeshield/log4jshell/Log4JDetectorTests.java
@@ -16,12 +16,14 @@ public class Log4JDetectorTests {
     assertTrue(checkResourceFile("/en16931-xml-validator-2.0.0-b2-jar-with-dependencies.jar"));
     assertTrue(checkResourceFile("/log4j-core-2.12.1.jar"));
     assertTrue(checkResourceFile("/log4j-core-2.14.1.jar"));
+    assertTrue(checkResourceFile("/log4j-core-2.15.0.jar"));
   }
 
   @Test
   public void checkSecure() throws IOException, URISyntaxException {
     assertFalse(checkResourceFile("/spring-boot-2.5.7.jar"));
-    assertTrue(checkResourceFile("/log4j-core-2.15.0.jar"));
+    assertFalse(checkResourceFile("/log4j-core-2.16.0.jar"));
+    assertFalse(checkResourceFile("/log4j-core-2.17.0.jar"));
   }
 
   private boolean checkResourceFile(String url) throws IOException, URISyntaxException {
